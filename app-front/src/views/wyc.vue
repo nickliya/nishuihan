ddd89c40c6994506b2f62614031ead387083762d
yangqing <401219180@qq.com>
2018-10-10 16:56:08 +0800

    新框架结构

---
+++ app-front/src/views/wyc.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+     <wycThird></wycThird>
+     <wycFifth></wycFifth>
+  </div>
+</template>
+
+<script>
+import wycThird from './wycThird.vue';
+import wycFifth from './wycFifth.vue';
+
+export default {
+  name: 'wyc',
+  data() {
+    return {
+    };
+  },
+  components: {
+    wycThird,
+    wycFifth,
+  },
+};
+
+</script>
+
+<style lang="less">
+@import '../static/csshake.css';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- app-front/src/views/wycFifth.vue
+++ app-front/src/views/wycFifth.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="skillobm">
     <a-row>
-      <a-col :span="8"><h3>开局五秒会有羽碎</h3></a-col>
-    </a-row>
-    <a-row>
-      <a-col :span="8"><a-button v-if="isEnd" v-on:click="dostart">开始计时</a-button>
-      <a-button v-else v-on:click="doend">结束计时</a-button></a-col>
-    </a-row>
-    <a-row>
       <a-col :span="8">
-        <a-tag v-if="yscount>3" color="#2db7f5" type="success">羽碎 {{yscount}}s</a-tag>
-        <a-tag v-else type="danger" color="#f50" class="shake shake-constant shake-constant--hover">
-         羽碎 {{yscount}}s</a-tag>
-      </a-col>
-    </a-row>
-    <a-row>
-      <a-col :span="8">
-        <a-tag v-if="sixDonecount>3" color="#2db7f5" type="success">六点一 {{sixDonecount}}s</a-tag>
-        <a-tag v-else type="danger" color="#f50" class="shake shake-constant shake-constant--hover">
-         六点一 {{sixDonecount}}s</a-tag>
+        <h3>6次普攻喷射一次</h3>
+        <h3>90% 大毒三点一 30秒一次</h3>
+        <h3>80% 柱子 120秒一次</h3>
+        <h3>70% 小怪 60秒一次</h3>
+        <h3>50% 大招 50秒一次</h3>
       </a-col>
     </a-row>
   </div>
