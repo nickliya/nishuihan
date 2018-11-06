d702dcb0d10f2d930a48cc2fdd8dbd7a92c2d90b
yangqing <401219180@qq.com>
2018-11-06 14:39:00 +0800

    修改eslint配置

--- app-front/.eslintrc.js
+++ app-front/.eslintrc.js
@@ -2,19 +2,35 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    "browser": true,
+    "commonjs": true,
+    "es6": true
   },
   plugins: [
-    "vue"
+    "vue",
+    "html"
   ],
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "linebreak-style": [0 ,"error", "windows"],
-    "max-len": ["error", { "code": 200 }]
+    "max-len": ["error", { "code": 200 }],
+    "indent": [
+      "error",
+      2
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        // "never"
+    ]
   },
   parserOptions: {
     parser: 'babel-eslint',
