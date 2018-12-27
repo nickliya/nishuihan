b8724627890885144eee79583457ea8b7fa98f01
yangqing <401219180@qq.com>
2018-12-27 11:30:50 +0800

    技能价格修改

--- app-front/src/views/skillPrice.vue
+++ app-front/src/views/skillPrice.vue
@@ -3,7 +3,8 @@
     <template slot="name" slot-scope="text">
       <a href="javascript:;">{{text}}</a>
     </template>
-    <template slot="title" slot-scope="currentPageData">
+    <template slot="title" slot-scope="text, record, index">
+      Head
       <a-button @click="dojiulin('jiulin')">{{ skilljson.jiulin.name }}</a-button>
       <a-button @click="dojiulin('shenxiang')">{{ skilljson.shenxiang.name }}</a-button>
       <a-button @click="dojiulin('suwen')">{{ skilljson.suwen.name }}</a-button>
@@ -11,12 +12,34 @@
       <a-button @click="dojiulin('tieyi')">{{ skilljson.tieyi.name }}</a-button>
       <a-button @click="dojiulin('suimeng')">{{ skilljson.suimeng.name }}</a-button>
     </template>
-    <template slot="footer" slot-scope="currentPageData">
+    <template slot="footer" slot-scope="text, record, index">
+      Footer
+    </template>
+    <!-- <template slot="operation" slot-scope="text, record">
+        <a-popconfirm
+          v-if="tableData.length"
+          title="Sure to delete?"
+          @confirm="() => onDelete(record.key)">
+          <a>Delete</a>
+        </a-popconfirm>
+    </template> -->
+    <template slot="operation" slot-scope="text, record, index">
+      <div class='editable-row-operations'>
+        <span v-if="record.editable">
+          <a @click="() => save(record.key)">Save</a>
+          <a-popconfirm title='Sure to cancel?' @confirm="() => cancel(record.key)">
+            <a>Cancel</a>
+          </a-popconfirm>
+        </span>
+        <span v-else>
+          <a @click="() => edit(record.key)">Edit</a>
+        </span>
+      </div>
     </template>
   </a-table>
 </template>
 <script>
-import { Table, Button } from 'ant-design-vue';
+import { Table, Button, Popconfirm} from 'ant-design-vue';
 import skilljson from '../static/json/skill.json';
 
 const columns = [{
@@ -28,18 +51,27 @@ const columns = [{
   title: 'Price',
   className: 'column-money',
   dataIndex: 'money',
+},
+{
+  title: 'operation',
+  dataIndex: 'operation',
+  scopedSlots: { customRender: 'operation' },
 }];
 
+const tableData=[]
 export default {
   components: {
     'a-table': Table,
     'a-button': Button,
+    'a-popconfirm': Popconfirm,
   },
   data() {
+    this.cacheData = tableData.map(item => ({ ...item }))
     return {
       skilljson,
       tableData: skilljson.jiulin.dataindex,
       columns,
+      count: 2,
     };
   },
   methods: {
@@ -47,12 +79,57 @@ export default {
       // console.log(skilljson);
       this.tableData = skilljson[val].dataindex;
     },
+    onDelete (key) {
+      const dataSource = [...this.tableData]
+      this.tableData = dataSource.filter(item => item.key !== key)
+    },
+    handleChange (value, key, column) {
+      const newData = [...this.tableData]
+      const target = newData.filter(item => key === item.key)[0]
+      if (target) {
+        target[column] = value
+        this.tableData = newData
+      }
+    },
+    edit (key) {
+      const newData = [...this.tableData]
+      const target = newData.filter(item => key === item.key)[0]
+      if (target) {
+        target.editable = true
+        this.tableData = newData
+      }
+    },
+    save (key) {
+      const newData = [...this.tableData]
+      const target = newData.filter(item => key === item.key)[0]
+      if (target) {
+        delete target.editable
+        this.tableData = newData
+        this.cacheData = newData.map(item => ({ ...item }))
+      }
+    },
+    cancel (key) {
+      const newData = [...this.tableData]
+      const target = newData.filter(item => key === item.key)[0]
+      if (target) {
+        Object.assign(target, this.cacheData.filter(item => key === item.key)[0])
+        delete target.editable
+        this.data = newData
+      }
+    },
   },
 };
+
 </script>
 <style>
 /* th.column-money,
 td.column-money {
   text-align: right !important;
 }; */
+</style>
+
+<style scoped>
+.editable-row-operations a {
+  margin-right: 8px;
+}
 </style>
